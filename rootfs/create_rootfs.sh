e001fe2282af1ab3412cb2def904925bce504251
Evgeny Boger <eugenyboger@gmail.com>
2013-10-07 00:02:57 +0400

    libnfc packages install

--- rootfs/create_rootfs.sh
+++ rootfs/create_rootfs.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
+ADD_PACKAGES="netbase,ifupdown,iproute,openssh-server,iputils-ping,wget,udev,net-tools,ntpdate,ntp,vim,nano,less,tzdata,console-tools,module-init-tools,mc,wireless-tools,usbutils,i2c-tools,isc-dhcp-client,wpasupplicant"
+REPO="http://ftp.debian.org/debian"
+OUTPUT="rootfs"
+RELEASE=wheezy
+
+
 if [ "$(id -u)" != "0" ]; then
 	echo "Sorry, you are not root."
 	echo "USAGE: sudo create_rootfs.sh"
 	exit 1
 fi
 
+# directly download firmware-realtek from non-free repo
+RTL_FIRMWARE_DEB="http://ftp.de.debian.org/debian/pool/non-free/f/firmware-nonfree/firmware-realtek_0.36+wheezy.1_all.deb"
 
-ADD_PACKAGES="netbase,ifupdown,iproute,openssh-server,iputils-ping,wget,udev,net-tools,ntpdate,ntp,vim,nano,less,tzdata,console-tools,module-init-tools,mc,wireless-tools,usbutils,i2c-tools,isc-dhcp-client,firmware-realtek,wpasupplicant"
-REPO="http://ftp.debian.org/debian"
-OUTPUT="rootfs"
-RELEASE=wheezy
 
 echo "Install dependencies"
 apt-get install qemu-user-static binfmt-support
 
 echo "Will create rootfs"
-debootstrap --include=${ADD_PACKAGES} --verbose --arch armel --variant=minbase --foreign ${RELEASE} ${OUTPUT} ${REPO}
+debootstrap --include=${ADD_PACKAGES} --verbose --arch armel  --variant=minbase --foreign ${RELEASE} ${OUTPUT} ${REPO}
 
 echo "Copy qemu to rootfs"
 cp /usr/bin/qemu-arm-static ${OUTPUT}/usr/bin
@@ -47,12 +51,25 @@ chroot ${OUTPUT}/ /usr/sbin/locale-gen
 LANG=en_US.UTF-8 sudo chroot ${OUTPUT}/ update-locale
 
 
-echo "Add rtl8188 hostapd package"
-RTL8188_DEB=rtl8188_hostapd/hostapd_1.1-rtl8188_armel.deb
-cp ../contrib/${RTL8188_DEB} ${OUTPUT}/
-chroot ${OUTPUT}/ dpkg -i ${RTL8188_DEB}
-chroot ${OUTPUT}/ rm ${RTL8188_DEB}
+#echo "Add rtl8188 hostapd package"
+#RTL8188_DEB=hostapd_1.1-rtl8188_armel.deb
+#cp ../contrib/rtl8188_hostapd/${RTL8188_DEB} ${OUTPUT}/
+#chroot ${OUTPUT}/ dpkg -i ${RTL8188_DEB}
+#chroot ${OUTPUT}/ rm ${RTL8188_DEB}
+
+echo "Add libnfc packages"
+mkdir ${OUTPUT}/tmp/libnfc
+cp ../contrib/libnfc/libnfc*.deb ${OUTPUT}/tmp/libnfc
+chroot ${OUTPUT}/ dpkg -i /tmp/libnfc/libnfc5_1.7.0-2_armel.deb /tmp/libnfc/libnfc-bin_1.7.0-2_armel.deb /tmp/libnfc/libnfc-examples_1.7.0-2_armel.deb
+rm -r ${OUTPUT}/tmp/libnfc
 
 
 
+echo "Install realtek firmware"
+chroot ${OUTPUT}/ wget ${RTL_FIRMWARE_DEB} -O rtl_firmware.deb
+chroot ${OUTPUT}/ dpkg -i rtl_firmware.deb
+chroot ${OUTPUT}/ rm rtl_firmware.deb
+
+echo "Overwrite configs one more time"
+cp -r configs/* ${OUTPUT}/
 
