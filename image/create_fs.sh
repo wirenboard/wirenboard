a2acdd6c43f586e37eca76b62516b1693e04fd99
Evgeny Boger <boger@contactless.ru>
2014-05-20 03:12:31 +0400

    script to create ext4 filesystem

---
+++ image/create_fs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "USAGE: $0 /dev/mmcblk0p2"
+
+if [ "x$1" = "x" ]; then
+    echo "please specify device";
+    exit 1;
+fi
+    
+sudo mkfs.ext4 $1 -E stride=2,stripe-width=1024 -b 4096 -L rootfs 262144
