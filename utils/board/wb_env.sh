a25833b14c5b7e44488d485c9d1e736edec163c5
Evgeny Boger <boger@contactless.ru>
2014-05-20 03:10:55 +0400

    add board-specific env settings

---
+++ utils/board/wb_env.sh
@@ -0,0 +1,97 @@
+#!/bin/sh
+case "`cat /proc/device-tree/model`" in
+    "Wiren Board rev. 3.2 (i.MX23)" )
+	WB_VERSION=32;
+
+	WB_GPIO_MUX_A=32;
+	WB_GPIO_MUX_B=33;
+	WB_GPIO_MUX_C=34;
+
+	WB_GPIO_PWR_33=39;
+	WB_GPIO_RFM_CS=51;
+	WB_GPIO_NRF_CS=35;
+	WB_GPIO_NRF_EN=37;
+
+	WB_GSM_POWER_TYPE=2
+	WB_GPIO_GSM_POWER=245;
+	WB_GPIO_GSM_PWRKEY=248;
+	WB_GPIO_GSM_STATUS=249;
+	WB_GPIO_RELAY_1=247;
+	WB_GPIO_RELAY_2=246;
+
+	WB_GPIO_FET_1=52;
+	WB_GPIO_FET_2=50;
+	WB_GPIO_FET_3=57;
+	WB_GPIO_FET_4=54;
+	WB_FET_COUNT=4;
+
+    WB_MUX_NAMES_0=( A1 )
+    WB_MUX_NAMES_1=( A2 )
+    WB_MUX_NAMES_2=( A3 )
+    WB_MUX_NAMES_3=( A4 )
+    WB_MUX_NAMES_4=( R1 )
+    WB_MUX_NAMES_5=( R2 )
+    WB_MUX_NAMES_6=( R3 )
+    WB_MUX_NAMES_7=( R4 )
+
+	WB_GPIO_A1=${WB_GPIO_FET_1};
+	WB_GPIO_A2=${WB_GPIO_FET_2};
+	WB_GPIO_A3=${WB_GPIO_FET_3};
+	WB_GPIO_A4=${WB_GPIO_FET_4};
+
+	WB_GPIO_W1=4
+	WB_GPIO_W1=2
+	WB_GPIO_W1=1
+
+
+    ;;
+    "Wiren Board rev. 2.8 (i.MX23)" )
+	WB_VERSION=28;
+	WB_GSM_POWER_TYPE=1
+
+	WB_GPIO_GSM_RESET=7;
+	WB_GPIO_GSM_PWRKEY=6;
+
+	WB_GPIO_MUX_A=36;
+	WB_GPIO_MUX_B=37;
+	WB_GPIO_MUX_C=38;
+
+	WB_GPIO_PWR_33=16;
+
+	WB_GPIO_TB2=32;
+	WB_GPIO_TB3=33;
+	WB_GPIO_TB4=34;
+	WB_GPIO_TB5=35;
+	WB_GPIO_TB6=39;
+	WB_GPIO_TB7=1;
+	WB_GPIO_TB19=60;
+
+	WB_GPIO_FET_1=${WB_GPIO_TB2};
+	WB_GPIO_FET_2=${WB_GPIO_TB3};
+	WB_GPIO_FET_3=${WB_GPIO_TB4};
+	WB_GPIO_FET_4=${WB_GPIO_TB5};
+	WB_GPIO_FET_5=${WB_GPIO_TB6};
+	WB_GPIO_FET_6=${WB_GPIO_TB7};
+	WB_GPIO_FET_7=${WB_GPIO_TB19};
+	WB_FET_COUNT=7;
+
+
+    WB_MUX_NAMES_0=( tb3 )
+    WB_MUX_NAMES_1=( tb4 )
+    WB_MUX_NAMES_2=( tb5 )
+    WB_MUX_NAMES_3=( tb2 )
+    WB_MUX_NAMES_4=( tb6 )
+    WB_MUX_NAMES_5=( vin )
+    WB_MUX_NAMES_6=( tb7 )
+    WB_MUX_NAMES_7=( tb9 )
+
+
+
+    ;;
+    * )
+	WB_VERSION="unknown";
+    ;;
+esac
+
+#~ echo "Wiren Board Version is: $WB_VERSION" 1>&2
+
