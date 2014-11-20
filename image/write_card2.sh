63d883bd6b34fd40e8b7bf9135f0bb814785717d
Evgeny Boger <boger@contactless.ru>
2014-11-20 19:38:55 +0300

    add temporary script to write card using /dev/sdX device

---
+++ image/write_card2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ "x$1" = "x" ]; then
+    echo "please provide the image location"
+    exit 1;
+fi
+if [ "x$2" = "x" ]; then
+    echo "please provide sdcard device prefix (example: /dev/sdX)"
+    exit 1;
+fi
+
+DEV=$2
+
+sudo umount ${DEV}2
+sudo umount ${DEV}1
+
+sudo dd if=$1 of=${DEV} bs=4M conv=fdatasync
+sync
+sync
+sudo parted ${DEV} -s "resizepart 2 -0"
