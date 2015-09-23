b671dcb71f5576911917750cd72133735f9b0000
Alexey Ignatov <lexszero@gmail.com>
2015-09-23 20:41:32 +0300

    install_update.sh: rewrite to new FIT images and split scripts
    See previous commit for detailed description

--- image/install_update.sh
+++ image/install_update.sh
@@ -1,93 +1,12 @@
-#!/bin/bash -x
-this="`readlink -f $0`"
+#!/bin/bash
+set -e
+set -x
 
-die() {
-	>&2 echo "!!! $@"
-	[[ $? == 0 ]] && exit 1 || exit $?
-}
-
-info() {
-	>&2 echo ">>> $@"
-}
-
-extract_blob_body() {
-	# extracts lines from $1 to $2, trimming trailing \n
-	local begin=$1
-	local end=$2
-
-	awk '
-		NR >= '$begin' {
-			q=p
-			p=$0
-		}
-		NR > '$begin' {
-			print q
-		}
-		NR == '$end' {
-			ORS=""
-			print p
-			exit 0
-		}' "$this"
-}
-
-# Find blob boundaries and expected MD5
-find_blob() {
-	local tag=$1
-
-	local tmp=`awk '
-		/^__BLOB_BEGIN__:'"$tag"':$/ {
-			begin=NR+1
-		}
-		match($0, /^__BLOB_END__:'"$tag"':([0-9a-f]+)$/, a) {
-			print begin, NR-1, a[1]
-			exit 0
-		}' "$this"`
-	local begin="${tmp%% * *}"
-	local end="${tmp#* }"
-	local md5_expected="${end#* }"
-	end="${end% *}"
-
-	[[ -n "$begin" && -n "$end" && -n "$md5_expected" ]] && {
-		echo "$tag $begin $end $md5_expected"
-	} || {
-		die "Blob $tag not found"
-	}
-}
-
-verify_blob() {
-	local tag=$1
-	local begin=$2
-	local end=$3
-	local md5_expected=$4
-
-	info "Checking MD5 checksum of $tag"
-	local md5_calculated=`extract_blob_body $begin $end | md5sum | cut -f1 -d' '`
-	[[ "$md5_expected" == "$md5_calculated" ]] &&
-		info "MD5 checksum of $tag ok" ||
-		die "MD5 of $tag doesn't match (expected $md5_expected, got $md5_calculated)"
-}
-
-extract_tarball() {
-	local tag=$1
-	local begin=$2
-	local end=$3
-	local dst=$5
-
-	info "Unpacking $tag into $dst"
-	pushd $dst
-	extract_blob_body $begin $end | tar xjvp
-	popd
-}
-
-[[ $EUID != 0 ]] && die "Need root privileges to install update"
-
-blob=`find_blob ROOTFS`
-verify_blob $blob
+fit_blob_verify_hash rootfs
 
 info "Installing firmware update"
 
-tmpdir='/dev/shm'
-mnt="$tmpdir/rootfs"
+MNT="$TMPDIR/rootfs"
 
 declare -a partitions=( 
 	''
@@ -99,31 +18,67 @@ declare -a partitions=(
 	'data'
 )
 
-root_dev='mmcblk0'
-part=`readlink /dev/root`
-part=${part##*${root_dev}p}
-case "$part" in
+ROOT_DEV='mmcblk0'
+PART=`readlink /dev/root`
+PART=${PART##*${ROOT_DEV}p}
+case "$PART" in
 	2)
-		part=3
+		PART=3
 		;;
 	3)
-		part=2
+		PART=2
 		;;
 	*)
-		die "Unable to determine second rootfs partition (current is $part)"
+		die "Unable to determine second rootfs PARTition (current is $PART)"
 		;;
 esac
-root_part=/dev/${root_dev}p${part}
-info "Will install to $root_part"
+ROOT_PART=/dev/${ROOT_DEV}p${PART}
+info "Will install to $ROOT_PART"
 
-umount -f $root_part 2&>1 >/dev/null || true # just for sure
-info "Formatting $root_part"
-yes | mkfs.ext4 -L "${partitions[$part]}" -E stride=2,stripe-width=1024 -b 4096 "$root_part" || die "mkfs.ext4 failed"
+umount -f $ROOT_PART 2&>1 >/dev/null || true # just for sure
+info "Formatting $ROOT_PART"
+yes | mkfs.ext4 -L "${partitions[$PART]}" -E stride=2,stripe-width=1024 -b 4096 "$ROOT_PART" || die "mkfs.ext4 failed"
 
-info "Mounting $root_part to $mnt"
-rm -rf "$mnt" && mkdir "$mnt" || die "Unable to create mountpoint $mnt"
-mount -t ext4 "$root_part" "$mnt" || die "Unable to mount just created filesystem"
+cleanup() {
+	set +e
+	info "Unmounting chroot mounts"
+	umount $MNT/mnt/data
+	[[ -L $MNT/dev/ptmx ]] || umount $MNT/dev/ptmx
+	umount $MNT/dev/pts
+	umount $MNT/dev
+	umount $MNT/sys
+	umount $MNT/proc
+	
+	info "Unmounting new rootfs"
+	umount $MNT
+	sync
+}
+trap cleanup EXIT
 
-extract_tarball $blob "$mnt"
+info "Mounting $ROOT_PART at $MNT"
+rm -rf "$MNT" && mkdir "$MNT" || die "Unable to create mountpoint $MNT"
+mount -t ext4 "$ROOT_PART" "$MNT" || die "Unable to mount just created filesystem"
 
-info "Done"
+info "Extracting files to new rootfs"
+pushd "$MNT"
+fit_blob_data rootfs | tar xJp
+popd
+
+info "Preparing chroot mounts"
+mount -t proc procfs $MNT/proc
+mount --bind /sys $MNT/sys
+mount --bind /dev $MNT/dev
+mount -t devpts devpts $MNT/dev/pts -o "gid=5,mode=620,ptmxmode=666,newinstance"
+[[ -L $MNT/dev/ptmx ]] || mount -o bind $MNT/dev/pts/ptmx $MNT/dev/ptmx
+mount --bind /mnt/data $MNT/mnt/data
+
+info "Running rc.local script in new rootfs"
+chroot $MNT /etc/rc.local
+
+cleanup
+
+info "Switching to new rootfs"
+fw_setenv mmcpart $PART
+
+info "Done, rebooting"
+reboot
