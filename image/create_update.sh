ecb7d87741d811390bbd34b02532b31a19d640de
Alexey Ignatov <lexszero@gmail.com>
2015-09-24 01:04:34 +0300

    create_update.sh: fix tarball creation

--- image/create_update.sh
+++ image/create_update.sh
@@ -33,7 +33,7 @@ create_tarball() {
 	local tarball=`mktemp`
 
 	pushd "$src" >/dev/null
-	sudo tar cJvp ./ > "$tarball" || {
+	sudo tar cJvp --numeric-owner ./ > "$tarball" || {
 		rm -f "$tarball"
 		die "tarball of $src creation failed"
 	}
