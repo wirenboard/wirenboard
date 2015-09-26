b927b2368978582a846377e3a45213abacd46b2e
Alexey Ignatov <lexszero@gmail.com>
2015-09-27 02:57:53 +0300

    chroot_this.sh: fix script

--- configs/configs/chroot_this.sh
+++ configs/configs/chroot_this.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
+set -x
+set -e
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo $DIR
 
-[[ $EUID == 0 ]] || {
-	exec sudo -E unshare -m $0 "$@"
+[[ -n "$__unshared" ]] || {
+	[[ $EUID == 0 ]] || {
+		exec sudo -E "$0" "$@"
+	}
+
+	# Jump into separate namespace
+	export __unshared=1
+	exec unshare -umipf "$0" "$@"
 }
 
-mount --bind /proc $DIR/proc
+mkdir -p "$DIR/{proc,sys,dev/pts}"
+mount -t proc proc $DIR/proc
 mount --bind /sys $DIR/sys
 mount --bind /dev $DIR/dev
 mount -t devpts devpts $DIR/dev/pts -o "gid=5,mode=620,ptmxmode=666,newinstance"
-[[ -L $DIR/dev/ptmx ]] || mount -o bind $DIR/dev/pts/ptmx $DIR/dev/ptmx
+[[ -L $DIR/dev/ptmx ]] || mount --bind $DIR/dev/pts/ptmx $DIR/dev/ptmx
+
+cleanup_mounts() {
+	umount -R "$DIR/dev"
+	umount "$DIR/proc"
+	umount "$DIR/sys"
+}
+trap cleanup_mounts EXIT
 
 chroot $DIR "$@"
