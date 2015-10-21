50b56ff38e916f111bfe5c2f1fdbf1043f3c1ec0
Evgeny Boger <boger@contactless.ru>
2015-10-21 03:07:21 +0300

    add ssmtp

--- rootfs/create_rootfs.sh
+++ rootfs/create_rootfs.sh
@@ -5,7 +5,7 @@ ADD_PACKAGES=netbase,ifupdown,iproute,openssh-server,iputils-ping,wget,udev,\
 net-tools,ntpdate,ntp,vim,nano,less,tzdata,console-tools,module-init-tools,mc,\
 wireless-tools,usbutils,i2c-tools,udhcpc,wpasupplicant,psmisc,curl,dnsmasq,gammu,\
 python-serial,memtester,apt-utils,dialog,locales,python3-minimal,unzip,minicom,\
-iw,ppp,libmodbus5,python-smbus
+iw,ppp,libmodbus5,python-smbus,ssmtp
 #REPO="http://ftp.debian.org/debian"
 REPO="http://mirror.yandex.ru/debian"
 OUTPUT="rootfs"
