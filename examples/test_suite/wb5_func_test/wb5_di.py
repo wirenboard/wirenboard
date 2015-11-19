a8af1b8eec1dfdfd1c4fb21632adf66787cdee79
Evgeny Boger <boger@contactless.ru>
2015-11-19 22:02:27 +0300

    add wb5 test suite

---
+++ examples/test_suite/wb5_func_test/wb5_di.py
@@ -0,0 +1,36 @@
+# coding: utf-8
+import unittest
+
+import os
+
+import sys; sys.path.insert(0, "../common")
+
+import time
+import wbmqtt
+import subprocess
+
+class TestFETs(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        subprocess.call("service wb-homa-adc start", shell=True)
+
+        #~ cls.adc = adc.ADC()
+        cls.wbmqtt = wbmqtt.WBMQTT()
+        
+    @classmethod
+    def tearDownClass(cls):
+        cls.wbmqtt.close()
+
+    def setUp(self):
+        self.wbmqtt.clear_values()
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ examples/test_suite/wb5_func_test/wb5_func_test.py
@@ -0,0 +1,163 @@
+
+import unittest
+from collections import OrderedDict
+import os
+import sys
+import time
+import subprocess
+sys.path.insert(0, "../common")
+
+import leds
+import gsm
+import w1
+
+
+#~ import gpio
+import rs485
+#~ import relay
+import network
+import beeper
+
+import wb5_adc
+import wb5_di
+import rf433
+import wifi
+import can
+from gpio import GPIO
+
+from gdocs import GSheetsLog
+
+
+class WB5TestW1(w1.TestW1):
+    NUMBER_REQUIRED = 1
+
+
+class WB5TestRS485(rs485.TestRS485):
+    port_1 = '/dev/ttyAPP1'
+    port_2 = '/dev/ttyAPP4'
+
+    @classmethod
+    def setUpClass(cls):
+        rs485.TestRS485.setUpClass()
+        subprocess.call("ifconfig can0 down", shell=True)
+        subprocess.call("ifconfig can1 down", shell=True)
+
+class WB5TestRFM69(rf433.TestRFM69):
+    SPI_MAJOR = 32765 # -1
+    SPI_MINOR = 0
+    IRQ_GPIO = 38
+
+
+
+gsm_test = gsm.TestGSMMTS
+# gsm_test = gsm.TestGSMegafon
+
+mapping = OrderedDict([
+    ( WB5TestRS485      , 8 ),
+    ( wifi.TestWifi , 9 ),
+    ( WB5TestRFM69  , 10 ),
+    ( wb5_adc.TestADC          , 6 ),
+    ( WB5TestW1            , 7 ),
+    ( network.TestNetwork,   3 ),
+    ( can.TestCAN,  4 ),
+    ( gsm_test          , 2 ),
+    ( gsm.TestGSMRTC        , 5 ),
+])
+
+
+
+def suite():
+    suite = unittest.TestSuite()
+
+    for test_class in mapping.iterkeys():
+        suite.addTest(unittest.makeSuite(test_class))
+
+    return suite
+
+
+
+
+def get_mac():
+   return os.popen('wb-gen-serial').read().strip()
+
+
+def print_sn(sn):
+    print "====================================="
+    print "IMEI SN:     %s %s      " % (str(sn)[:3], str(sn)[3:])
+    print "====================================="
+
+
+
+
+if __name__ == '__main__':
+    #~ leds.set_brightness('green', 0)
+    #~ leds.set_brightness('red', 0)
+
+    #~ relay.init()
+
+    subprocess.call("killall -9 wb-rules", shell=True)
+    #~ subprocess.call("killall -9 wb-rules", shell=True)
+
+    beep = beeper.Beeper(3)
+    beep.setup()
+    beep.test()
+
+
+    gsm.init_gsm()
+    imei = gsm.gsm_get_imei()
+    print "imei=%s" % imei
+
+
+    mac = get_mac()
+
+    result = unittest.TextTestRunner(verbosity=2).run(suite())
+
+
+    results_row = ['--', ]  * (max(mapping.values()) + 1)
+
+    for test_class, test_index in mapping.iteritems():
+        results_row[test_index] = 'OK'
+
+    for test, err_msg in (result.errors + result.failures):
+        test_index = mapping[test.__class__]
+        results_row[test_index] = 'FAIL'
+
+
+    #~ adc_cal = wb4_adc.AdcCalibrate()
+    #~ print "r1 constants for R1 and R2 channels:", adc_cal.get_r1_calib(), adc_cal.get_r2_calib()
+
+    #~ results_row += [str(adc_cal.get_r1_calib()), str(adc_cal.get_r2_calib())]
+
+    #~ results_row.append(MEM_TYPE)
+
+
+
+    overall_status = 'OK' if result.wasSuccessful() else 'FAIL'
+
+
+    prefix, sn, crc = GSheetsLog.split_imei(imei)
+
+    print "====================================="
+    print "Overall status:    %s    " % overall_status
+    print "====================================="
+    print_sn(sn)
+
+    if len(result.errors + result.failures) == 0:
+        leds.set_brightness('red', 0)
+        leds.blink_fast('green')
+    else:
+        pass
+        leds.blink_fast('red')
+        leds.set_brightness('green', 0)
+
+
+
+    print "sending data to google..."
+
+    log = GSheetsLog('https://docs.google.com/spreadsheets/d/1wKNCMss9ZSyhtr0GFNvRgaGyw2RRPn9weE8w7qjxHiw/edit#gid=0', '../common/Commissioning-30b68b322b7c.json')
+    log.update_data(imei, overall_status, [mac,] +  results_row)
+
+
+
+
+    print "Done!"
