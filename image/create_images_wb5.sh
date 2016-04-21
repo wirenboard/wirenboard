3b76aa18710f2c9c2d7c39ef075305671ff48659
Evgeny Boger <boger@contactless.ru>
2016-04-21 16:05:19 +0300

    restore old behaviour of the script

--- image/create_images_wb5.sh
+++ image/create_images_wb5.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-if [ $# -lt 1 ] || [ $# -gt 2 ] ; then
-    echo "USAGE <path to rootfs> [version]"
+if [ $# -lt 2 ] || [ $# -gt 3 ] ; then
+    echo "USAGE <path to rootfs> <tag> [fw version]"
 	exit 1
 fi
 
@@ -11,30 +11,26 @@ set_fdt() {
 }
 
 ROOTFS=$1
+TAG=$2
 
 VERSION=`cat "$ROOTFS/etc/wb-fw-version"` || die "Unable to get firmware version"
 echo "FW version: $VERSION"
 
-if [ ! -z "$2" ]; then
-    VERSION=$2
+if [ ! -z "$3" ]; then
+    VERSION=$3
     echo "FW version overriden: $VERSION"
 fi
 
-REVISIONS="52 55"
+
 OUT_DIR="image/wb5/${VERSION}"
 mkdir -p ${OUT_DIR}
-for ver in ${REVISIONS}; do
-    set_fdt imx28-wirenboard${ver} ${ROOTFS}
-    IMG_NAME="${OUT_DIR}/${VERSION}_emmc_wb${ver}.img"
-    WEBUPD_NAME="${OUT_DIR}/${VERSION}_webupd_wb${ver}.fit"
+IMG_NAME="${OUT_DIR}/${VERSION}_emmc_wb${TAG}.img"
+WEBUPD_NAME="${OUT_DIR}/${VERSION}_webupd_wb${TAG}.fit"
 
-    rm -f ${IMG_NAME}
-    ./create_image.sh mx28 ${ROOTFS} ../contrib/u-boot/u-boot.wb5.sd  ${IMG_NAME}
-    zip ${IMG_NAME}.zip ${IMG_NAME}
-    ./create_update.sh ${ROOTFS} ${WEBUPD_NAME}
-done
+rm -f ${IMG_NAME}
+./create_image.sh mx28 ${ROOTFS} ../contrib/u-boot/u-boot.wb5.sd  ${IMG_NAME}
+zip ${IMG_NAME}.zip ${IMG_NAME}
+./create_update.sh ${ROOTFS} ${WEBUPD_NAME}
 
 echo "Done"
 echo  ${OUT_DIR}
-
-
