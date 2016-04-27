da82adab93740074ca95eb3f5bdf1da67bd3b3b4
Ivan Shvedunov <ivan4th@gmail.com>
2016-04-27 18:35:18 +0300

    devenv: fix initial ~/wbdev dir creation.

--- devenv/wbdev_profile.sh
+++ devenv/wbdev_profile.sh
@@ -7,7 +7,7 @@ export GOPATH="$WORKSPACE_DIR/go"
 export PATH="/usr/local/go/bin:$GOPATH/bin:$PATH"
 mkdir -p "$GOPATH"
 if [ -n "$DEV_USER" ]; then
-    chown "$DEV_USER.$DEV_USER" "$GOPATH"
+    chown "$DEV_USER.$DEV_USER" "$WORKSPACE_DIR" "$GOPATH"
 fi
 if [ -f /.devdir ]; then
     cd "$(cat /.devdir)"
