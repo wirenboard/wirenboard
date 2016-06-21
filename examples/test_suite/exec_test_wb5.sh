a23dd9c04f0351958644c173287421e72164f1a6
Evgeny Boger <boger@contactless.ru>
2016-06-21 13:47:46 +0300

    update test script

--- examples/test_suite/exec_test_wb5.sh
+++ examples/test_suite/exec_test_wb5.sh
@@ -9,6 +9,10 @@ fi
 
 HOST=$1
 CREDENTIALS_FNAME=Commissioning-30b68b322b7c.json
+PASSWORD="wirenboard"
 
-scp common/${CREDENTIALS_FNAME} root@${HOST}:/dev/shm/credentials.json
-ssh -t root@${HOST} "ln -s -f /dev/shm/credentials.json /usr/lib/wb-test-suite/common/${CREDENTIALS_FNAME}; service ntp restart; cd /usr/lib/wb-test-suite/wb5_func_test; bash"
+echo "Connecting to $HOST ..."
+
+ssh-keygen -f ~/.ssh/known_hosts -R $HOST
+sshpass -p ${PASSWORD} scp  -o StrictHostKeyChecking=no common/${CREDENTIALS_FNAME} root@${HOST}:/dev/shm/credentials.json
+sshpass -p ${PASSWORD} ssh  -o StrictHostKeyChecking=no -t root@${HOST} "ln -s -f /dev/shm/credentials.json /usr/lib/wb-test-suite/common/${CREDENTIALS_FNAME}; service ntp restart; cd /usr/lib/wb-test-suite/wb5_func_test; bash"
