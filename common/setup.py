798ed8abc874307be510f334be26f34daeb20025
Nikita webconn Maslov <wkernelteam@gmail.com>
2016-07-11 12:04:23 +0300

    move 'common' from test_suite to separate package
    Signed-off-by: Nikita webconn Maslov <wkernelteam@gmail.com>

---
+++ common/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import find_packages
+
+
+setup(name          =   "wb_common",
+      version       =   "1.0.0",
+      author        =   "Evgeny Boger",
+      #package_dir   =   {"wb_common": "src"},
+      packages      =   find_packages())
