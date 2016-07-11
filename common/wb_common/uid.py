07aedb9adffebad6931b7436c1802f87bbe564ed
Nikita webconn Maslov <wkernelteam@gmail.com>
2016-07-11 13:45:09 +0300

    move get_mac() from wb-common to test_suite
    Remove circular dependency between these packages
    
    Signed-off-by: Nikita webconn Maslov <wkernelteam@gmail.com>

--- common/wb_common/uid.py
+++ common/wb_common/uid.py
@@ -1,6 +1,10 @@
 import re
 import os
 
+# get_mac() has been moved to test_suite, because it uses wb-gen-serial,
+# which depends on this (wb-common) package (avoid circular dependency)
+
+
 def get_cpuinfo_serial():
     data = open('/proc/cpuinfo').read()
     matches = re.findall('^Serial\s+: ([0-9a-f]+)$', data, re.M)
@@ -22,10 +26,7 @@ def get_mmc_serial():
                     return serial
     return None
 
-def get_mac():
-    return os.popen('wb-gen-serial').read().strip()
-
 
 if __name__ == '__main__':
     print "/proc/cpuinfo serial: ", get_cpuinfo_serial()
-    print "WB serial (eth mac): ", get_mac()
+    #print "WB serial (eth mac): ", get_mac()
