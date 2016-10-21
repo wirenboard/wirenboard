af5ae396fc9e4540887280784b4cbe9f21e01204
Nikita webconn Maslov <n.maslov@contactless.ru>
2016-10-21 17:50:36 +0300

    devenv: install Git from backports
    Signed-off-by: Nikita webconn Maslov <n.maslov@contactless.ru>

--- devenv/prep.sh
+++ devenv/prep.sh
@@ -11,10 +11,13 @@ services_disable
 
 /bin/echo -e 'APT::Get::Assume-Yes "true";\nAPT::Get::force-yes "true";' >/rootfs/etc/apt/apt.conf.d/90forceyes
 chr apt-get update
-chr apt-get install -y devscripts python-virtualenv git equivs build-essential \
+chr apt-get install -y devscripts python-virtualenv equivs build-essential \
     libmosquittopp-dev libmosquitto-dev pkg-config gcc-4.7 g++-4.7 libmodbus-dev \
     libwbmqtt-dev libcurl4-gnutls-dev libsqlite3-dev bash-completion \
     valgrind libgtest-dev google-mock cmake liblircclient-dev liblog4cpp5-dev python-setuptools
+
+# install git from backports to support desktop latest Git configs
+chr apt-get install -y -t wheezy-backports git git-man
 
 (rm -rf /rootfs/dh-virtualenv && cd /rootfs && git clone https://github.com/spotify/dh-virtualenv.git && cd dh-virtualenv && git checkout 0.10)
 chr bash -c "cd /dh-virtualenv && mk-build-deps -ri && dpkg-buildpackage -us -uc -b"
