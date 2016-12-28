7d328cfe4c1b301a0f81a68d0faf66e48f8edc0d
Evgeny Boger <boger@contactless.ru>
2016-12-28 21:45:20 +0300

    test-suite: update ADC test to work with new wbmqtt

--- examples/test_suite/wb5_func_test/wb5_adc.py
+++ examples/test_suite/wb5_func_test/wb5_adc.py
@@ -5,7 +5,10 @@ import unittest
 from wb_common import wbmqtt
 import subprocess
 import time
-
+import os.path
+from shutil import copyfile
+import json
+import re
 
 def test():
     return False
@@ -15,14 +18,30 @@ class TestADCBase(unittest.TestCase):
     ref_5v = 4.2
     @classmethod
     def setUpClass(cls):
-        subprocess.call("service wb-homa-adc start", shell=True)
+        tmp_conf_fname = '/var/run/shm/adc-test.conf'
+        subprocess.call("killall -9 wb-homa-adc", shell=True)
 
-        #~ cls.adc = adc.ADC()
+        # turn off averaging and oversampling on each channel
+        adc_conf_json = open('/etc/wb-homa-adc.conf').read()
+        adc_conf_json = re.sub('//.*', '', adc_conf_json)
+        adc_conf = json.loads(adc_conf_json)
+        for chan in adc_conf['iio_channels']:
+            chan['averaging_window'] = 1
+            chan['readings_number'] = 1
+
+        json.dump(adc_conf, open(tmp_conf_fname, 'wt'))
+        cls.adc_driver = subprocess.Popen("wb-homa-adc -c %s" % tmp_conf_fname, shell=True)
+
+
         cls.wbmqtt = wbmqtt.WBMQTT()
+        cls.wbmqtt.watch_device('wb-adc')
+        cls.wbmqtt.watch_device('wb-gpio')
+
 
     @classmethod
     def tearDownClass(cls):
         cls.wbmqtt.close()
+        cls.adc_driver.kill()
 
     def setUp(self):
         self.wbmqtt.send_value('wb-gpio', '5V_OUT', '1')
@@ -33,19 +52,33 @@ class TestADCBase(unittest.TestCase):
 
         self.assertAlmostEqual(v_measured, v_ref, delta=v_ref * delta_percent / 100.0)
 
+    def _adc_assert_less(self, adc_control, v_max, tries=3):
+        for i in xrange(tries):
+            v_measured = float(self.wbmqtt.get_next_value('wb-adc', adc_control))
+            if v_measured < v_max:
+                break
+        self.assertLess(v_measured, v_max)
+
+    def _adc_assert_greater(self, adc_control, v_min, tries=3):
+        for i in xrange(tries):
+            v_measured = float(self.wbmqtt.get_next_value('wb-adc', adc_control))
+            if v_measured > v_min:
+                break
+        self.assertGreater(v_measured, v_min)
+
     def _test_a1a4(self, fet_control, di_control, adc_control):
-        self.wbmqtt.send_value('wb-gpio', fet_control, '1')
-        time.sleep(500E-3)
+        self.wbmqtt.send_and_wait_for_value('wb-gpio', fet_control, '1')
 
         v_max = 0.20  # V
-        v_measured = float(self.wbmqtt.get_next_value('wb-adc', adc_control))
-        self.assertLess(v_measured, v_max)
+
+        self._adc_assert_less(adc_control, v_max, tries=3)
 
         di_state = bool(int(self.wbmqtt.get_last_value('wb-gpio', di_control)))
         self.assertFalse(di_state)
 
-        self.wbmqtt.send_value('wb-gpio', fet_control, '0')
-        time.sleep(100E-3)
+
+        self.wbmqtt.send_and_wait_for_value('wb-gpio', fet_control, '0')
+
         self._test_adc_value(adc_control, self.ref_5v, 4)
 
         di_state = bool(int(self.wbmqtt.get_last_value('wb-gpio', di_control)))
@@ -97,16 +130,13 @@ class TestADC55(TestADCBase):
             self.ref_5v = float(v5_val_str)
 
     def test_5vout(self):
-        self.wbmqtt.send_value('wb-gpio', '5V_OUT', '0')
-        time.sleep(500E-3)
-        v_measured = float(self.wbmqtt.get_next_value('wb-adc', '5Vout'))
-        self.assertLess(v_measured, 3.0)
+        self.wbmqtt.send_and_wait_for_value('wb-gpio', '5V_OUT', '0')
 
-        self.wbmqtt.send_value('wb-gpio', '5V_OUT', '1')
-        time.sleep(500E-3)
-        v_measured = float(self.wbmqtt.get_next_value('wb-adc', '5Vout'))
-        self.assertLessEqual(v_measured, 5.6)
-        self.assertGreater(v_measured, 4.8)
+        self._adc_assert_less('5Vout', 3.0, tries=3)
+
+        self.wbmqtt.send_and_wait_for_value('wb-gpio', '5V_OUT', '1')
+        self._adc_assert_less('5Vout', 5.6)
+        self._adc_assert_greater('5Vout', 4.8)
 
 if __name__ == '__main__':
     print "Usage: python %s [TestADC52|TestADC55]" % sys.argv[0]
