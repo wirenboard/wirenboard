79a5aa4df0dc1c4933ad641425e9ecbde6b1f135
Alexey Ignatov <lexszero@gmail.com>
2017-02-01 11:31:27 +0200

    rootfs: wb6 kludge for busybox-syslogd

--- rootfs/boards/6.sh
+++ rootfs/boards/6.sh
@@ -7,4 +7,7 @@ board_install() {
 	chr_apt wb-mqtt-homeui wb-rules wb-rules-system netplug wb-test-suite wb-hwconf-manager
 
 	set_fdt imx6ul-wirenboard-evk
+
+	# FIXME: armhf busybox-syslogd don't supports -s option, so here is a temporary kludge
+	sed -r -i 's/(SYSLOG_OPTS=).*/\1""/' $OUTPUT/etc/default/busybox-syslogd
 }
