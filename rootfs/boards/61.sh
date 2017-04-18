ad0864421d7105c1871089c5df2a61d17f93ae59
Alexey Ignatov <lexszero@gmail.com>
2017-04-18 12:58:49 +0300

    Support building WB61 rootfs

---
+++ rootfs/boards/61.sh
@@ -0,0 +1,13 @@
+# Wiren Board 6.1
+export FORCE_WB_VERSION=61
+
+. ${SCRIPT_DIR}/boards/include/soc_imx6ul.sh
+
+board_install() {
+	install_wb5_packages
+
+	set_fdt imx6ul-wirenboard61
+
+	# FIXME: armhf busybox-syslogd don't supports -s option, so here is a temporary kludge
+	sed -r -i 's/(SYSLOG_OPTS=).*/\1""/' $OUTPUT/etc/default/busybox-syslogd
+}
