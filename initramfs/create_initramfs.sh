6c160efe92c6cdf0d6d3bb17a0da2a38e670a550
Alexey Ignatov <lexszero@gmail.com>
2017-05-13 01:14:05 +0300

    initramfs: get more bins from rootfs along with required shared libs

--- initramfs/create_initramfs.sh
+++ initramfs/create_initramfs.sh
@@ -43,6 +43,15 @@ install_from_rootfs() {
 		shift
 	}
 	install_file "$ROOTFS/$src" "$dst"
+
+	# If file is executable, need to get its shared lib dependencies too
+	if [[ -x "$ROOTFS/$src" ]]; then
+		chroot "$ROOTFS" ldd "$src" |
+		sed -rn 's#[^/]*(/[^ ]*).*#\1#p' |
+		while read lib; do
+			[[ -e "$INITRAMFS/$lib" ]] || install_from_rootfs "$lib"
+		done
+	fi
 }
 
 rm -rf "$INITRAMFS"
@@ -57,6 +66,20 @@ install_dir "/usr/sbin"
 
 mknod "$INITRAMFS/dev/console" c 5 1
 
-install_from_rootfs /bin/busybox
 install_file "$FILES_DIR/init" "/init"
 install_file "$FILES_DIR/fstab" "/etc/fstab"
+
+FROM_ROOTFS=(
+	/bin/busybox
+	/bin/bash
+	/usr/bin/fw_printenv
+	/usr/bin/fw_setenv
+	/etc/fw_env.config
+	/usr/bin/fit_info
+	/sbin/mkfs.ext4
+	/usr/bin/wb-run-update
+)
+
+for f in "${FROM_ROOTFS[@]}"; do
+	install_from_rootfs "$f"
+done
