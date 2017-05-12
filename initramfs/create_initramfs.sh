12c4efb86c2a0c68df1ec2052a65466d7665cc43
Alexey Ignatov <lexszero@gmail.com>
2017-05-13 01:12:46 +0300

    initramfs: some cleanups of create_initramfs.sh

--- initramfs/create_initramfs.sh
+++ initramfs/create_initramfs.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+set -e
+#set -x
+
 [[ "$#" == "2" ]] || {
 	echo "Usage: $0 <rootfs> <initramfs_dir>"
 	exit 1
@@ -15,36 +18,42 @@ SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 TOP_DIR="$(readlink -f "$SCRIPT_DIR/..")"
 FILES_DIR="$SCRIPT_DIR/files"
 
+install_dir() {
+	echo "dir $1"
+	mkdir -p "$INITRAMFS/$1"
+}
+
 install_file() {
 	local src="$1"
 	local dst="$2"
-	shift 2
 	
-	echo "$dst <- $src"
-	install -D -T "$@" "$src" "$INITRAMFS/$dst"
+	local dstdir=$(dirname "$dst")
+	[[ -d "$INITRAMFS/$dstdir" ]] || install_dir "$dstdir"
+	
+	echo "file $dst <- $src"
+	cp "$src" "$INITRAMFS/$dst"
 }
 
 install_from_rootfs() {
 	local src="$1"
 	local dst="$2"
-	shift
 
 	[[ -z "$dst" ]] && {
 		dst="$src"
 		shift
 	}
-	install_file "$ROOTFS/$src" "$dst" "$@"
+	install_file "$ROOTFS/$src" "$dst"
 }
 
 rm -rf "$INITRAMFS"
 
-mkdir -p "$INITRAMFS/dev"
-mkdir -p "$INITRAMFS/proc"
-mkdir -p "$INITRAMFS/sys"
+install_dir "/dev"
+install_dir "/proc"
+install_dir "/sys"
 
-mkdir -p "$INITRAMFS/sbin"
-mkdir -p "$INITRAMFS/usr/bin"
-mkdir -p "$INITRAMFS/usr/sbin"
+install_dir "/sbin"
+install_dir "/usr/bin"
+install_dir "/usr/sbin"
 
 mknod "$INITRAMFS/dev/console" c 5 1
 
