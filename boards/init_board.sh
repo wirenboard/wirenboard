bb9833e0c645842ec7d621778c214e72359eaf57
Alexey Ignatov <lexszero@gmail.com>
2017-05-30 20:25:23 +0300

    rootfs: install packages listed in BOARD_PACKAGES array

--- boards/init_board.sh
+++ boards/init_board.sh
@@ -7,6 +7,8 @@ board_include() {
 	source "$BOARDS_DIR/include/$1"
 }
 
+export BOARD_PACKAGES=()
+
 [[ -e "${BOARDS_DIR}/${BOARD}.sh" ]] && . "${BOARDS_DIR}/${BOARD}.sh" || {
 	echo "Unknown board $BOARD"
 	echo "Please specify one of:"
