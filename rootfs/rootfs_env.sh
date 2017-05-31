5ecd4d46693fb1028ae87c41bff005caef5fa4e2
Alexey Ignatov <lexszero@gmail.com>
2017-05-31 04:56:44 +0300

    rootfs_env: add unexpected errors handler to fail more verbosely

--- rootfs/rootfs_env.sh
+++ rootfs/rootfs_env.sh
@@ -68,6 +68,26 @@ dbg() {
     chr ls -l /proc
 }
 
+error_trace() {
+	exec >&2
+	local ret=$?
+	set +o xtrace
+	local code="${1:-1}"
+	echo "Error in ${BASH_SOURCE[1]}:${BASH_LINENO[0]}. '${BASH_COMMAND}' exited with status $ret"
+	# Print out the stack trace described by $function_stack  
+	if [[ ${#FUNCNAME[@]} -gt 2 ]]; then
+		echo "Call tree:"
+		for (( i = 1; i < ${#FUNCNAME[@]} - 1; i++)); do
+			echo " $i: ${BASH_SOURCE[$i+1]}:${BASH_LINENO[$i]} ${FUNCNAME[$i]}(...)"
+		done
+	fi
+	echo "Exiting with status ${code}"
+	exit "${code}"
+}
+
+trap 'error_trace' ERR
+set -o errtrace
+
 die() {
 	local ret=$?
 	>&2 echo "!!! $@"
