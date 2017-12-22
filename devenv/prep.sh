08bef82292c4cc337ccb17c180134bd39cecc4d3
attiladoor <attiladoor@gmail.com>
2017-12-22 17:34:04 +0300

    add prep.sh

--- devenv/prep.sh
+++ devenv/prep.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -u -e
 
-ROOTFS_DIR=${ROOTFS:-"/rootfs"}
+ROOTFS=${ROOTFS:-"/rootfs"}
 SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
 . "$SCRIPT_DIR"/rootfs/rootfs_env.sh
@@ -9,31 +9,50 @@ SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 prepare_chroot
 services_disable
 
-/bin/echo -e 'APT::Get::Assume-Yes "true";\nAPT::Get::force-yes "true";' >$ROOTFS_DIR/etc/apt/apt.conf.d/90forceyes
-chr apt-get update
-chr apt-get install -y devscripts python-virtualenv equivs build-essential \
-    libmosquittopp-dev libmosquitto-dev pkg-config gcc-4.7 g++-4.7 libmodbus-dev \
+/bin/echo -e 'APT::Get::Assume-Yes "true";\nAPT::Get::force-yes "true";' >$ROOTFS/etc/apt/apt.conf.d/90forceyes
+chr apt-get update --allow-unauthenticated
+
+pkgs=(devscripts python-virtualenv equivs build-essential \
+    libmosquittopp-dev libmosquitto-dev pkg-config libmodbus-dev \
     libwbmqtt-dev libcurl4-gnutls-dev libsqlite3-dev bash-completion \
-    valgrind libgtest-dev google-mock cmake liblircclient-dev liblog4cpp5-dev python-setuptools \
-    cdbs libpng12-dev libqt4-dev autoconf automake libtool libpthsem-dev libpthsem20 \
-    libusb-1.0-0-dev knxd-dev knxd-tools \
-    cdbs libpng12-dev libqt4-dev linux-headers-4.1.15-imxv5-x0.1
+    libgtest-dev google-mock cmake liblircclient-dev python-setuptools \
+    cdbs libqt4-dev autoconf automake libtool libpthsem-dev libpthsem20 \
+    libusb-1.0-0-dev knxd-dev knxd-tools knxd \
+    cdbs libqt4-dev linux-headers-4.1.15-imxv5-x0.1 git git-man
+)
 
-# install git from backports to support desktop latest Git configs
-chr apt-get install -y -t wheezy-backports git git-man
+if [[ ${RELEASE} == "wheezy" ]]; then
+	chr_apt --force-yes "${pkgs[@]}"
+elif [[ ${RELEASE} == "stretch" ]]; then
+	chr apt-get install  --force-yes --allow-unauthenticated "${pkgs[@]}"
+fi
 
-(rm -rf $ROOTFS_DIR/dh-virtualenv && cd $ROOTFS_DIR && git clone https://github.com/spotify/dh-virtualenv.git && cd dh-virtualenv && git checkout 0.10)
+if [[ ${RELEASE} == "wheezy" ]]; then
+chr apt-get install -y gcc-4.7 g++-4.7 libpng12-dev valgrind
+elif [[ ${RELEASE} == "stretch" ]]; then
+    chr apt-get install -y --allow-unauthenticated gcc g++ libpng-dev
+fi
+
+##fix me
+echo "/lib/arm-linux-gnueabi" >> /etc/ld.so.conf.d/multiarch.conf
+echo "/usr/lib/arm-linux-gnueabi" >> /etc/ld.so.conf.d/multiarch.conf
+echo "/usr/arm-linux-gnueabi/lib" >>  /etc/ld.so.conf.d/multiarch.conf
+# for wb-mqtt-knx
+ln -s /usr/lib/x86_64-linux-gnu/libeibclient.so.0 /usr/lib/x86_64-linux-gnu/libeibclient.so
+
+(rm -rf $ROOTFS/dh-virtualenv && cd $ROOTFS && git clone https://github.com/spotify/dh-virtualenv.git && cd dh-virtualenv && git checkout 0.10)
 chr bash -c "cd /dh-virtualenv && mk-build-deps -ri && dpkg-buildpackage -us -uc -b"
 chr bash -c "dpkg -i /dh-virtualenv_*.deb"
 
 # build and install google test and google mock
 chr bash -c "cd /usr/src/gtest && cmake . && make && mv libg* /usr/lib/"
 
-cp /usr/src/gmock/CMakeLists.txt $ROOTFS_DIR/usr/src/gmock
+cp /usr/src/gmock/CMakeLists.txt $ROOTFS/usr/src/gmock
+chr bash -c "ln -s /usr/src/gtest /usr/src/gmock/gtest"
 chr bash -c "cd /usr/src/gmock && cmake . && make && mv libg* /usr/lib/"
 
 
-cp /etc/profile.d/wbdev_profile.sh $ROOTFS_DIR/etc/profile.d/
+cp /etc/profile.d/wbdev_profile.sh $ROOTFS/etc/profile.d/
 
 chr apt-get clean
-rm -rf $ROOTFS_DIR/dh-virtualenv
+rm -rf $ROOTFS/dh-virtualenv
