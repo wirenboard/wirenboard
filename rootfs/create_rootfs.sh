2d22c00e5b9cc537efb040cf596fc01654c63e07
attiladoor <attiladoor@gmail.com>
2018-01-15 13:51:01 +0300

    auto kernel flavor select

--- rootfs/create_rootfs.sh
+++ rootfs/create_rootfs.sh
@@ -240,7 +240,7 @@ if [[ ${RELEASE} == "wheezy" ]]; then
     chr_apt --force-yes "${pkgs[@]}"
 elif [[ ${RELEASE} == "stretch" ]]; then
     chr apt-get update --allow-unauthenticated
-    chr_apt --force-yes linux-image-wb2 device-tree-compiler=1.4.1+wb20170426233333 libssl1.0-dev systemd-sysv
+    chr_apt --force-yes linux-image-${KERNEL_FLAVOUR} device-tree-compiler=1.4.1+wb20170426233333 libssl1.0-dev systemd-sysv
     chr_apt --allow-unauthenticated --force-yes --force-confnew "${pkgs[@]}"
 fi
 #chr mv /etc/apt/sources.list.d/local.list /etc/apt/sources.list.d/contactless.list
