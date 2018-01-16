6c8baa19d887fe1cc3717b0c642eb9ea7c2d59fc
attiladoor <attiladoor@gmail.com>
2018-01-16 11:30:10 +0300

    removed auto installed repo conf

--- rootfs/create_rootfs.sh
+++ rootfs/create_rootfs.sh
@@ -242,6 +242,8 @@ elif [[ ${RELEASE} == "stretch" ]]; then
     chr apt-get update --allow-unauthenticated
     chr_apt --force-yes linux-image-${KERNEL_FLAVOUR} device-tree-compiler=1.4.1+wb20170426233333 libssl1.0-dev systemd-sysv
     chr_apt --allow-unauthenticated --force-yes "${pkgs[@]}"
+    rm -f ${OUTPUT}/etc/apt/sources.list.d/contactless*
+    chr apt-get update --allow-unauthenticated
 fi
 #chr mv /etc/apt/sources.list.d/local.list /etc/apt/sources.list.d/contactless.list
 # stop mosquitto on host
