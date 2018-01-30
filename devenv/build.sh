12da92204ddf77ffcc3ac6235a0866a2448503f0
attiladoor <attiladoor@gmail.com>
2018-01-30 15:14:48 +0300

    wheezy is added to build.sh

--- devenv/build.sh
+++ devenv/build.sh
@@ -10,8 +10,9 @@ do_build() {
 	/root/prep.sh
 }
 
-do_build wheezy armhf 6
+do_build stretch armel 5
 do_build wheezy armel 5
+do_build wheezy armhf 6
 
 # TBD: run chroot:
 # proot -R /rootfs -q qemu-arm-static -b /home/ivan4th /bin/bash
