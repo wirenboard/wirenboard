ed3c5cbfcca153719139e04b48f58b8a74dbf924
attiladoor <attiladoor@gmail.com>
2018-03-29 11:01:31 +0300

    build all rootfs

--- devenv/build.sh
+++ devenv/build.sh
@@ -12,6 +12,8 @@ do_build() {
 
 do_build stretch armel 5
 do_build stretch armhf 6
+do_build wheezy armel 5
+do_build wheezy armhf 6
 
 # TBD: run chroot:
 # proot -R /rootfs -q qemu-arm-static -b /home/ivan4th /bin/bash
