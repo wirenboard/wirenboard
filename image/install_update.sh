9b8caba49667fbdc174db294d79e5444373e016b
Nikita webconn Maslov <wkernelteam@gmail.com>
2018-05-23 15:26:53 +0300

    image: wipe partition on user request in FIT
    Signed-off-by: Nikita webconn Maslov <wkernelteam@gmail.com>

--- image/install_update.sh
+++ image/install_update.sh
@@ -2,6 +2,17 @@
 set -e
 
 
+# FIXME: move these lines to config, wb_env.sh maybe
+HIDDENFS_PART=/dev/mmcblk0p1
+HIDDENFS_OFFSET=$((8192*512))  # 8192 blocks of 512 bytes
+DEVCERT_NAME="device.crt.pem"
+INTERM_NAME="intermediate.crt.pem"
+ROOTFS_CERT_PATH="/etc/ssl/certs/device_bundle.crt.pem"
+
+DATA_WIPE_BUTTON_TIMEOUT=10
+DATA_PART=/dev/mmcblk0p6
+DATA_LABEL="data"
+
 check_compatible() {
 	local fit_compat=`fit_prop / compatible`
 	[[ -z "$fit_compat" || "$fit_compat" == "unknown" ]] && return 0
@@ -9,6 +20,13 @@ check_compatible() {
 		[[ "$fit_compat" == "$compat" ]] && return 0
 	done
 	return 1
+}
+
+mkfs_ext4() {
+    local part=$1
+    local label=$2
+
+    yes | mkfs.ext4 -L "$label" -E stride=2,stripe-width=1024 -b 4096 "$part"
 }
 
 if flag_set "force-compatible"; then
@@ -64,7 +82,7 @@ flag_set "from-initramfs" && {
 # determine if new partition is unformatted
 [[ "x`blkid $ROOT_PART | sed 's/^.*TYPE="\(.*\)".*$/\1/'`" != "xext4" ]] && {
     info "Formatting $ROOT_PART"
-    yes | mkfs.ext4 -L "$PARTLABEL" -E stride=2,stripe-width=1024 -b 4096 "$ROOT_PART" || die "mkfs.ext4 failed"
+    mkfs_ext4 $ROOT_PART $PARTLABEL || die "mkfs.ext4 failed"
 }
 
 umount -f $ROOT_PART 2&>1 >/dev/null || true # just for sure
@@ -99,6 +117,21 @@ sync; sync
 info "Switching to new rootfs"
 fw_setenv mmcpart $PART
 fw_setenv upgrade_available 1
+
+# ask user if he wants to wipe data partition
+flag_set "from-initramfs" && {
+    info "If you want to wipe data partition of this controller, press the button within $DATA_WIPE_BUTTON_TIMEOUT seconds"
+    info "DANGER: THIS CAN NOT BE UNDONE"
+    button_wait $DATA_WIPE_BUTTON_TIMEOUT && {
+        info "Again, after next button press your data will be removed!"
+        info "Press the button again if you're ABSOLUTELY sure"
+        button_wait $DATA_WIPE_BUTTON_TIMEOUT {
+            info "Formatting /mnt/data..."
+            umount -f $DATA_PART # just to be sure
+            mkfs_ext4 $DATA_PART $DATA_LABEL || die "Failed to format data partition"
+        }
+    } || info "Data wiping aborted"
+}
 
 info "Done, removing firmware image and rebooting"
 rm_fit
