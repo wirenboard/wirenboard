3291f504a9f1df61297c00e32b8a164502ffa099
Nikita webconn Maslov <wkernelteam@gmail.com>
2018-06-22 20:43:40 +0300

    image: start moving some definitions to wb-run-update
    Signed-off-by: Nikita webconn Maslov <wkernelteam@gmail.com>

--- image/install_update.sh
+++ image/install_update.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 set -e
 
-type fit_prop_string 2>/dev/null | grep -q 'shell function' || {
-    fit_prop_string() {
-        fit_prop "$@" | tr -d '\0'
-    }
-}
-
-# FIXME: move these lines to config, wb_env.sh maybe
+# FIXME: transitional definitions which are being moved to wb-run-update
 HIDDENFS_PART=/dev/mmcblk0p1
 HIDDENFS_OFFSET=$((8192*512))  # 8192 blocks of 512 bytes
 DEVCERT_NAME="device.crt.pem"
 INTERM_NAME="intermediate.crt.pem"
 ROOTFS_CERT_PATH="/etc/ssl/certs/device_bundle.crt.pem"
 
-DATA_WIPE_BUTTON_TIMEOUT=10
-DATA_PART=/dev/mmcblk0p6
-DATA_LABEL="data"
+type fit_prop_string 2>/dev/null | grep -q 'shell function' || {
+    fit_prop_string() {
+        fit_prop "$@" | tr -d '\0'
+    }
+}
+
+type mkfs_ext4 2>/dev/null | grep -q 'shell function' || {
+    mkfs_ext4() {
+        local part=$1
+        local label=$2
+
+        yes | mkfs.ext4 -L "$label" -E stride=2,stripe-width=1024 -b 4096 "$part"
+    }
+}
+
 
 check_compatible() {
 	local fit_compat=`fit_prop_string / compatible`
@@ -25,13 +31,6 @@ check_compatible() {
 		[[ "$fit_compat" == "$compat" ]] && return 0
 	done
 	return 1
-}
-
-mkfs_ext4() {
-    local part=$1
-    local label=$2
-
-    yes | mkfs.ext4 -L "$label" -E stride=2,stripe-width=1024 -b 4096 "$part"
 }
 
 if flag_set "force-compatible"; then
@@ -141,23 +140,6 @@ sync; sync
 info "Switching to new rootfs"
 fw_setenv mmcpart $PART
 fw_setenv upgrade_available 1
-
-# ask user if he wants to wipe data partition
-if flag_set "from-initramfs"; then
-    info "If you want to wipe data partition of this controller, press the button within $DATA_WIPE_BUTTON_TIMEOUT seconds"
-    info "DANGER: THIS CAN NOT BE UNDONE"
-    if button_wait $DATA_WIPE_BUTTON_TIMEOUT; then
-        info "Again, after next button press your data will be removed!"
-        info "Press the button again if you're ABSOLUTELY sure"
-        if button_wait $DATA_WIPE_BUTTON_TIMEOUT; then
-            info "Formatting /mnt/data..."
-            umount -f $DATA_PART # just to be sure
-            mkfs_ext4 $DATA_PART $DATA_LABEL || die "Failed to format data partition"
-        fi
-    else
-        info "Data wiping aborted"
-    fi
-fi
 
 info "Done, removing firmware image and rebooting"
 rm_fit
