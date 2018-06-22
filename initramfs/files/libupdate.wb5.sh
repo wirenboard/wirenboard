f5c8c8a26bd39a6a613bb0354128b909cae08ab0
Nikita webconn Maslov <wkernelteam@gmail.com>
2018-06-22 22:53:36 +0300

    initramfs: add delay in update scripts to wait for PWM to settle
    Signed-off-by: Nikita webconn Maslov <wkernelteam@gmail.com>

--- initramfs/files/libupdate.wb5.sh
+++ initramfs/files/libupdate.wb5.sh
@@ -8,7 +8,7 @@ PERIOD=333333
 
 buzzer_init() {
     echo $PWM_BUZZER > /sys/class/pwm/pwmchip0/export 2>/dev/null || true
-    while [[ ! -f /sys/class/pwm/pwmchip0/pwm${PWM_BUZZER}/duty_cycle ]]; do sleep 0.1; done
+    sleep 1 # make PWM settle, presence of these files is not enough
 
     echo $DUTY_CYCLE > /sys/class/pwm/pwmchip0/pwm${PWM_BUZZER}/duty_cycle
     echo $PERIOD > /sys/class/pwm/pwmchip0/pwm${PWM_BUZZER}/period
