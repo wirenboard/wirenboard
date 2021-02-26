71e30685088117e7cfc9d099d01d6db934fb4650
PeteK12 <65652863+PeteK12@users.noreply.github.com>
2021-02-26 22:39:01 +0500

    Do not install busybox-syslogd for stretch
    Since wb-configs 1.84.0 rsyslog is used in stretch instead of busybox-syslogd. rsyslog and busybox-syslogd are installed as dependencies of wb-configs-stretch and wb-configs-wheezy. So busybox-syslogd is removed from common installation list during creation of rootfs. busybox-syslogd specific configs are also removed from 6.x boards.

--- boards/60.sh
+++ boards/60.sh
@@ -7,7 +7,4 @@ board_install() {
 	install_wb5_packages
 
 	set_fdt imx6ul-wirenboard-evk
-
-	# FIXME: armhf busybox-syslogd don't supports -s option, so here is a temporary kludge
-	sed -r -i 's/(SYSLOG_OPTS=).*/\1""/' $OUTPUT/etc/default/busybox-syslogd
 }
