9bd832bdeb7efa540282605a4c51f66496523941
Nikita Maslov <nikita.maslov@wirenboard.ru>
2021-04-30 22:41:51 +0300

    ci: add missing WIRENBOARD_BRANCH param to publishImage

--- ci/pipelines/publishImage.groovy
+++ ci/pipelines/publishImage.groovy
@@ -4,6 +4,7 @@ pipeline {
     }
     parameters {
         string(name: 'IMAGE_BUILDNUMBER', defaultValue: '', description: 'from pipelines/build-image')
+        string(name: 'WIRENBOARD_BRANCH', defaultValue: 'feature/32254-releases', description: 'wirenboard/wirenboard repo branch')
         booleanParam(name: 'SET_LATEST', defaultValue: false, description: 'remove saved rootfs images before build')
         booleanParam(name: 'PUBLISH_IMG', defaultValue: false, description: 'works with SET_LATEST=true')
     }
