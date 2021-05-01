5dba4f53eeed93154d78c0b406dcbb6171d9f826
Nikita Maslov <nikita.maslov@wirenboard.ru>
2021-05-01 20:12:53 +0300

    ci: add releaseImages routing pipeline

---
+++ ci/pipelines/releaseImages.groovy
@@ -0,0 +1,63 @@
+def imagesJobs = []
+
+pipeline {
+    agent {
+        label "devenv"
+    }
+    parameters {
+        string(name: 'BOARDS', defaultValue: '6x 67', description: 'space-separated list')
+        string(name: 'WIRENBOARD_BRANCH', defaultValue: 'feature/32254-releases', description: 'wirenboard/wirenboard repo branch')
+        string(name: 'WB_RELEASE', defaultValue: 'stable', description: 'wirenboard release (from WB repo)')
+        booleanParam(name: 'CLEANUP_ROOTFS', defaultValue: false, description: 'remove saved rootfs images before build')
+        string(name: 'WBDEV_IMAGE', defaultValue: 'contactless/devenv:latest', description: 'tag for wbdev')
+    }
+    stages {
+        stage('Build images') {
+            steps {
+                script {
+                    def boards = params.BOARDS.split(' ')
+
+                    for (board in boards) {
+                        def currentBoard = board
+                        def imageJob = build(
+                            job: 'pipelines/build-image',
+                            wait: true,
+                            parameters: [
+                                string(name: 'BOARD', value: currentBoard),
+                                string(name: 'WIRENBOARD_BRANCH', value: params.WIRENBOARD_BRANCH),
+                                string(name: 'WB_RELEASE', value: params.WB_RELEASE),
+                                string(name: 'WBDEV_IMAGE', value: params.WBDEV_IMAGE),
+                                booleanParam(name: 'CLEANUP_ROOTFS', value: params.CLEANUP_ROOTFS)
+                            ]
+
+                        imagesJobs.add(imageJob)
+                    }
+                }
+            }
+        }
+        stage('Publish images') {
+            steps {
+                script {
+                    def jobs = [:]
+
+                    for (job in imagesJobs) {
+                        def currentImageJob = job
+                        jobs["publish ${currentImageJob.getId()}"] = {
+                            stage("Publish ${currentImageJob.getId()}") {
+                                build job: 'pipelines/publish-image',
+                                      wait: true,
+                                      parameters: [
+                                        string(name: 'IMAGE_BUILDNUMBER', value: currentImageJob.getId()),
+                                        booleanParam(name: 'SET_LATEST', value: true),
+                                        booleanParam(name: 'PUBLISH_IMG', value: false)
+                                      ]
+                            }
+                        }
+                    }
+
+                    parallel jobs
+                }
+            }
+        }
+    }
+}
