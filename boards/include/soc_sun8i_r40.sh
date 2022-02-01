efdf4527fbbfbf322af4922507b98d511b0adefd
Maslov Nikita <webconn@users.noreply.github.com>
2022-02-01 18:12:55 +0300

    image: use u-boot from rootfs if available (#90)

--- boards/include/soc_sun8i_r40.sh
+++ boards/include/soc_sun8i_r40.sh
@@ -2,4 +2,5 @@ export ARCH=armhf
 export KERNEL_FLAVOUR=wb7
 export IMAGE_TYPE=sun8i_r40
 export U_BOOT=contrib/u-boot/u-boot.wb7.bin
+export U_BOOT_ROOTFS=usr/lib/u-boot/sun8i_wirenboard7/u-boot-sunxi-with-spl.bin
 export REPO_PLATFORM=wb7
