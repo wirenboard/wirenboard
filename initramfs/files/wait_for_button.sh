9824825ca48b7a664b5c8e9eb094db2f059ad4f5
Evgeny Boger <boger@wirenboard.com>
2022-02-09 20:30:11 +0300

    initramfs: wb: do not ask for confirmation before regular update
    we no longer ask for confirmation for auto-discovered update
    .fit files on USB and microSD media.
    
    We also indicate that valid .fit file is discovered by two short beeps
    to give a feedback to user without debug console attached.

--- initramfs/files/wait_for_button.sh
+++ initramfs/files/wait_for_button.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source /lib/libupdate.sh
+
 # Time to wait for button press in seconds
 WAIT_TIME=${WAIT_TIME:-10}
 
@@ -20,47 +22,6 @@ TICK_MUL=10
 
 use_buzzer() {
     [ "x${USE_BUZZER}" == "xy" ]
-}
-
-LIBS_PATH=/lib/
-DT_COMPAT_LIST=`tr < /proc/device-tree/compatible '\000' '\n'`
-
-for compat in $DT_COMPAT_LIST; do
-    case "$compat" in
-        "contactless,imx28-wirenboard50" )
-            LIB=wb5
-            break
-            ;;
-        "contactless,imx28-wirenboard52" )
-            LIB=wb5
-            break
-            ;;
-        "contactless,imx28-wirenboard55" )
-            LIB=wb5
-            break
-            ;;
-        "contactless,imx28-wirenboard58" )
-            LIB=wb5
-            break
-            ;;
-        "contactless,imx6ul-wirenboard60" )
-            LIB=wb6
-            break
-            ;;
-        "contactless,imx6ul-wirenboard61" )
-            LIB=wb6
-            break
-            ;;
-        "wirenboard,wirenboard-700" )
-            LIB=wb7
-            break
-            ;;
-    esac
-done
-
-source ${LIBS_PATH}/libupdate.${LIB}.sh || {
-    echo "Unknown platform"
-    exit 1
 }
 
 if [ "x${USE_ECHO}" == "xy" ]; then
