b12c7dcd97ce53166620a125ff615d7a3ceefe3e
Maslov Nikita <webconn@users.noreply.github.com>
2022-06-15 20:18:57 +0300

    prepare for new production process (#99)
    * initramfs: add missing tools for the new initenv process
      * initramfs: set explicit MAC for g_ether
      * image: add install_update.sh flags to support installation on bare
        Wiren Board

--- initramfs/files/usb_net.sh
+++ initramfs/files/usb_net.sh
@@ -3,7 +3,11 @@
 case $1 in
 up)
     touch /var/leases
-    modprobe g_ether
+
+    # Set explicit MAC addresses in order to have a consistent interface name.
+    # Without explicit addresses g_ether generates random ones.
+    modprobe g_ether dev_addr=12:34:56:78:9a:bc host_addr=12:34:56:78:9a:bd
+
     ifconfig usb0 192.168.41.1 netmask 255.255.255.0 up
     udhcpd
     while ! ping -c 1 192.168.41.2; do true; done; echo 'Host is online'
