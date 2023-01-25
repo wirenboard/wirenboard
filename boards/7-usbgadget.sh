07b26fe86dc17005c8986bb699bc1b912f359ca3
vdromanov <vdromanov@yahoo.com>
2023-01-25 16:01:56 +0300

    add separate dts for wb7-usbgadget (with high usb speed) (#125)
    * add separate dts for wb7-usbgadget (with high usb speed)
    
    * add forgotten set_fdt

--- boards/7-usbgadget.sh
+++ boards/7-usbgadget.sh
@@ -1,11 +1,11 @@
 # Wiren Board 7.2 and newer
 export FORCE_WB_VERSION=72
-export DTB=/boot/dtbs/sun8i-r40-wirenboard720.dtb
+export DTB=/boot/dtbs/sun8i-r40-wirenboard72x-initram.dtb
 
 board_include soc_sun8i_r40.sh
 
 board_install() {
     chr_apt dropbear mmc-utils rsync dosfstools
 
-	set_fdt sun8i-r40-wirenboard720
+	set_fdt sun8i-r40-wirenboard72x-initram
 }
