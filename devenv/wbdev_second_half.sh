8d1d78b185e1bb8f0238b03c45d918969facb47b
Nikita Maslov <nikita.maslov@wirenboard.ru>
2023-03-20 22:30:55 +0600

    devenv: add custom volume settings to call in DinD mode

--- devenv/wbdev_second_half.sh
+++ devenv/wbdev_second_half.sh
@@ -19,6 +19,27 @@ else
 fi
 
 PREFIX="$VM_HOME/wbdev/go/src/github.com/contactless"
+VOLUMES=""
+
+if [[ -n "$DEV_VOLUME" ]]; then
+    DEV_VOLUME_PREFIX="${DEV_VOLUME##*:}"
+
+    case "$PWD" in
+    "$DEV_VOLUME_PREFIX"*)
+        ;;
+    *)
+        echo "DEV_VOLUME is set; PWD must start with $DEV_VOLUME_PREFIX"
+        exit 1
+        ;;
+    esac
+
+    VOLUMES="$VOLUMES -v $DEV_VOLUME"
+    DEV_DIR="${PWD}"
+elif [[ -z "$DEV_DIR" ]]; then
+    DEV_DIR="$PREFIX/${PWD##*/}"
+    VOLUMES="$VOLUMES -v $HOME:$VM_HOME -v ${PWD%/*}:$PREFIX"
+fi
+
 
 ENV_CMDLINE=""
 for var in $(env | grep -o "WBDEV_[^=]*"); do
@@ -28,13 +49,12 @@ done
 docker run $DOCKER_TTY_OPTS --privileged --rm \
        -e DEV_UID=$UID \
        -e DEV_USER=$USER \
-       -e DEV_DIR="$PREFIX/${PWD##*/}" \
+       -e DEV_DIR="$DEV_DIR" \
        -e DEV_TERM="$TERM" \
        $ENV_CMDLINE \
        -e DEB_BUILD_OPTIONS \
        -e DEB_BUILD_PROFILES \
-       -v $HOME:$VM_HOME \
-       -v ${PWD%/*}:$PREFIX \
+       $VOLUMES \
        $ssh_opts \
        -h wbdevenv \
        $WBDEV_IMAGE \
