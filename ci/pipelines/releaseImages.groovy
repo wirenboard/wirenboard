655c142ece09e9c1555060020c9014209b4b53ce
Maslov Nikita <webconn@users.noreply.github.com>
2023-05-10 18:50:57 +0600

    ci: test images before publish (#148)
    * releaseImages: add step to test new images
    
    * build images in parallel
    
    * add customizeable TEST_JOB
    
    * fix publish
    
    * ci: use wbdev in publishImage
    
    * use production orchestrator job

--- ci/pipelines/releaseImages.groovy
+++ ci/pipelines/releaseImages.groovy
@@ -19,6 +19,9 @@ pipeline {
         string(name: 'WB_RELEASE', defaultValue: 'stable', description: 'wirenboard release (from WB repo)')
         booleanParam(name: 'CLEANUP_ROOTFS', defaultValue: false, description: 'remove saved rootfs images before build')
         string(name: 'WBDEV_IMAGE', defaultValue: 'contactless/devenv:latest', description: 'tag for wbdev')
+        string(name: 'TEST_JOB', defaultValue: 'wirenboard/wb-release-tests/main')
+        booleanParam(name: 'TEST_FACTORYRESET', defaultValue: true, description: 'run factory reset tests on images (wb7 only)')
+        booleanParam(name: 'TEST_STANDALONE', defaultValue: true, description: 'run generic FIT update tests on images')
     }
     triggers {
         parameterizedCron('''
@@ -30,24 +33,63 @@ pipeline {
             steps {
                 script {
                     def boards = params.BOARDS.split(' ')
+                    def jobs = [:]
 
                     for (board in boards) {
                         def currentBoard = board
-                        def imageJob = build(
-                            job: 'pipelines/build-image',
-                            wait: true,
-                            parameters: [
-                                string(name: 'DEBIAN_RELEASE', value: params.DEBIAN_RELEASE),
-                                string(name: 'BOARD', value: currentBoard),
-                                string(name: 'WIRENBOARD_BRANCH', value: params.WIRENBOARD_BRANCH),
-                                string(name: 'WB_RELEASE', value: params.WB_RELEASE),
-                                string(name: 'WBDEV_IMAGE', value: params.WBDEV_IMAGE),
-                                booleanParam(name: 'CLEANUP_ROOTFS', value: params.CLEANUP_ROOTFS),
-                                booleanParam(name: 'SAVE_ARTIFACTS', value: true)
-                            ])
-
-                        imagesJobs.add(imageJob)
+                        jobs["build ${currentBoard}"] = {
+                            stage("Build image for ${currentBoard}") { script {
+                                def imageJob = build(
+                                    job: 'pipelines/build-image',
+                                    wait: true,
+                                    parameters: [
+                                        string(name: 'DEBIAN_RELEASE', value: params.DEBIAN_RELEASE),
+                                        string(name: 'BOARD', value: currentBoard),
+                                        string(name: 'WIRENBOARD_BRANCH', value: params.WIRENBOARD_BRANCH),
+                                        string(name: 'WB_RELEASE', value: params.WB_RELEASE),
+                                        string(name: 'WBDEV_IMAGE', value: params.WBDEV_IMAGE),
+                                        booleanParam(name: 'CLEANUP_ROOTFS', value: params.CLEANUP_ROOTFS),
+                                        booleanParam(name: 'SAVE_ARTIFACTS', value: true)
+                                    ])
+                                imagesJobs.add([ job: imageJob, board: currentBoard ])
+                            }}
+                        }
                     }
+
+                    parallel jobs
+                }
+            }
+        }
+        stage('Test images') {
+            when { expression { params.TEST_FACTORYRESET || params.TEST_STANDALONE } }
+            steps {
+                script {
+                    def jobs = [:]
+
+                    for (item in imagesJobs) {
+                        def currentImageJob = item.job
+                        def currentBoard = item.board
+
+                        jobs["test ${currentImageJob.getId()}"] = {
+                            stage("Test ${currentImageJob.getId()}") {
+                                build(job: 'pipelines/release-test-orchestrator',
+                                      wait: true,
+                                      parameters: [
+                                        string(name: 'BENCH_BOARD', value: currentBoard),
+                                        string(name: 'FIT_BUILDID', value: currentImageJob.getId()),
+                                        string(name: 'WIRENBOARD_BRANCH', value: params.WIRENBOARD_BRANCH),
+                                        string(name: 'WBDEV_IMAGE', value: params.WBDEV_IMAGE),
+                                        string(name: 'TEST_JOB', value: params.TEST_JOB),
+                                        booleanParam(name: 'RUN_FACTORYRESET', value: params.TEST_FACTORYRESET && currentBoard == '7x'),
+                                        booleanParam(name: 'RUN_STANDALONE', value: params.TEST_STANDALONE),
+                                        booleanParam(name: 'RUN_RELEASE', value: false),  // suitable for release updates, not image publish
+                                        booleanParam(name: 'RUN_LEGACY', value: false),  // suitable for 6x boards and not for image publish
+                                      ])
+                            }
+                        }
+                    }
+
+                    parallel jobs
                 }
             }
         }
@@ -56,8 +98,8 @@ pipeline {
                 script {
                     def jobs = [:]
 
-                    for (job in imagesJobs) {
-                        def currentImageJob = job
+                    for (item in imagesJobs) {
+                        def currentImageJob = item.job
                         jobs["publish ${currentImageJob.getId()}"] = {
                             stage("Publish ${currentImageJob.getId()}") {
                                 build(job: 'pipelines/publish-image',
