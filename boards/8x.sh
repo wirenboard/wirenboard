cace9240d23cab4c4cab0ac623230cfb8aae9436
Nikita Maslov <nikita.maslov@wirenboard.com>
2024-03-20 20:05:09 +0500

    fix dtb location

--- boards/8x.sh
+++ boards/8x.sh
@@ -1,6 +1,6 @@
 # Wiren Board 8.0 and newer
 export FORCE_WB_VERSION=
-export DTB=/boot/dtbs/sun50i-h616-wirenboard8xx.dtb
+export DTB=/boot/dtbs/allwinner/sun50i-h616-wirenboard8xx.dtb
 
 board_include soc_sun50i_h616.sh
 
