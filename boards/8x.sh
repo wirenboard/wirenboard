412f6d96606e24bb606821d7e1b866c4cb79e4cb
vdromanov <vdromanov@yahoo.com>
2024-05-17 17:20:32 +0300

    wb8: change default dts (wirenboard8xx -> wirenboard84x) (#195)

--- boards/8x.sh
+++ boards/8x.sh
@@ -1,11 +1,11 @@
 # Wiren Board 8.0 and newer
 export FORCE_WB_VERSION=
-export DTB=/boot/dtbs/allwinner/sun50i-h616-wirenboard8xx.dtb
+export DTB=/boot/dtbs/allwinner/sun50i-h616-wirenboard843.dtb
 
 board_include soc_sun50i_h616.sh
 
 board_install() {
 	wb-common_install
 
-	set_fdt sun50i-h616-wirenboard8xx
+	set_fdt sun50i-h616-wirenboard843
 }
