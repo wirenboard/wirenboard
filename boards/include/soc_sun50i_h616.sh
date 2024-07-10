5c0375b9f285e84d28a718641543d5718dbf0417
Nikolay Korotkiy <sikmir@disroot.org>
2024-07-10 22:20:48 +0400

    image: add task-wirenboard-wb8 for wb8

--- boards/include/soc_sun50i_h616.sh
+++ boards/include/soc_sun50i_h616.sh
@@ -5,3 +5,5 @@ export U_BOOT=contrib/u-boot/u-boot.wb8.bin
 export U_BOOT_ROOTFS=usr/lib/u-boot/sun50i_wirenboard8/u-boot-sunxi-with-spl.bin
 export REPO_PLATFORM=wb8
 export BOARD_BOOTLET_IN_ROOTFS=y
+
+BOARD_PACKAGES+=( task-wirenboard-wb8 )
