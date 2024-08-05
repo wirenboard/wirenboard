bb959d110b2afc93cf7a5161ac398a1194704709
vdromanov <vdromanov@yahoo.com>
2024-08-05 12:55:11 +0300

    boards/8x: set dtb to common with 8x & 85x

--- boards/8x.sh
+++ boards/8x.sh
@@ -1,6 +1,9 @@
 # Wiren Board 8.0 and newer
+# DTB here is a bootlet-dtb (common with wb8x & wb85x)
+# All hardware-dependent services build actual configs at runtime
+# => common dtb here will not affect config-install-magic, while building rootfs
 export FORCE_WB_VERSION=
-export DTB=/boot/dtbs/allwinner/sun50i-h616-wirenboard843.dtb
+export DTB=/boot/dtbs/allwinner/sun50i-h616-wirenboard8xx.dtb
 
 board_include soc_sun50i_h616.sh
 
