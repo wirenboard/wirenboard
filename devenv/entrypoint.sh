184f6047a5f77947e204ea6e0a80be1ebca5f793
Nikolay Korotkiy <sikmir@disroot.org>
2024-09-06 17:18:55 +0400

    devenv: fix compiledb

--- devenv/entrypoint.sh
+++ devenv/entrypoint.sh
@@ -399,10 +399,7 @@ case "$cmd" in
         chr apt-get update
         chr mk-build-deps -ir -t "apt-get --force-yes -y"
         if [ -f CMakeLists.txt ]; then
-            mkdir -p build
-            pushd build
-            chu cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
-            popd
+            chu mkdir -p build; ( cd build && cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON .. )
             mv build/compile_commands.json compile_commands.json
         elif [ -f Makefile ]; then
             chu make -Bnwk | compiledb -o compile_commands.json
