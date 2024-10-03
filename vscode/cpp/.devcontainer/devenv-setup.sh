2270fb90710464d63d86f7e1fad2f50ed94c49bf
Nikolay Korotkiy <sikmir@disroot.org>
2024-10-03 09:28:25 +0400

    vscode: lcov -> gcovr (#215)

--- vscode/cpp/.devcontainer/devenv-setup.sh
+++ vscode/cpp/.devcontainer/devenv-setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 schroot -c bullseye-amd64-sbuild --directory=/ -- bash -c 'echo "deb http://deb.wirenboard.com/wb7/bullseye unstable main" > /etc/apt/sources.list.d/wirenboard-unstable.list'
-schroot -c bullseye-amd64-sbuild --directory=/ -- apt-get update 
-schroot -c bullseye-amd64-sbuild --directory=/ -- apt-get -y install libwbmqtt1-4:armhf libwbmqtt1-4-dev:armhf libwbmqtt1-4-test-utils:armhf j2cli:all gdbserver:armhf lcov libdb5.3++-dev:armhf libsqlite3-dev:armhf libsqlite3-0:armhf
+schroot -c bullseye-amd64-sbuild --directory=/ -- apt-get update
+schroot -c bullseye-amd64-sbuild --directory=/ -- apt-get -y install gdbserver:armhf gcovr:all j2cli:all
+schroot -c bullseye-amd64-sbuild --directory=/ -- apt-get -y install libwbmqtt1-5-test-utils:armhf libdb5.3++-dev:armhf libsqlite3-dev:armhf
 
 dir=$(pwd)
 schroot -c bullseye-amd64-sbuild --directory=/ -- mkdir -p $dir
