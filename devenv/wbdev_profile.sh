a5483bf97be2b73c4d7c7f2acc30423ba419466e
Nikolay Korotkiy <sikmir@disroot.org>
2024-10-25 20:50:46 +0400

    devenv: fix wbdev_profile.sh

--- devenv/wbdev_profile.sh
+++ devenv/wbdev_profile.sh
@@ -3,6 +3,7 @@ if [ -z "$DEV_USER" ]; then
 else
     export WORKSPACE_DIR="/home/$DEV_USER/wbdev"
 fi
+mkdir -p "$WORKSPACE_DIR"
 if [ -n "$DEV_USER" ]; then
     chown "$DEV_USER.$DEV_USER" "$WORKSPACE_DIR"
 fi
