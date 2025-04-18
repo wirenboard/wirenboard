29981af7c90a0ada8e96bd9c8a876431e004ea6f
Nikolay Korotkiy <sikmir@disroot.org>
2025-04-18 11:24:41 +0400

    ci: uploadDeb: support archives (#236)

--- ci/pipelines/uploadDeb.groovy
+++ ci/pipelines/uploadDeb.groovy
@@ -8,7 +8,7 @@ pipeline {
     label 'devenv'
   }
   parameters {
-    stashedFile 'upload.deb'
+    stashedFile(name: 'UPLOAD', description: '*.deb or *.tar* (archive with *.deb files)')
     choice(name: 'REPO', choices: ['release', 'devtools'], description: 'which repo to publish to')
     booleanParam(name: 'FORCE_OVERWRITE', defaultValue: false,
                  description: 'use only you know what you are doing, replace existing version of package')
@@ -24,8 +24,10 @@ pipeline {
     }
     stage('Prepare file') {
       steps { dir("$FILES_DIR") {
-        unstash 'upload.deb'
-        sh 'wbdev user dpkg-name upload.deb'
+        unstash 'UPLOAD'
+        sh 'mv UPLOAD $UPLOAD_FILENAME'
+        sh 'find . -type f -name "*.tar*" -exec tar -xf {} --strip-components=1 \\;'
+        sh 'wbdev user find . -maxdepth 1 -type f -name "*.deb" -exec dpkg-name {} \\;'
         archiveArtifacts artifacts: '*.deb'
       }}
     }
