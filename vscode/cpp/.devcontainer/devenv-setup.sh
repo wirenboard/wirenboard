d1a575933994cafaf838fda286b2e30958965f94
Илья <mail@u236.org>
2025-08-19 11:41:35 +0300

    devcontainer: add automatic build depends install feature

--- vscode/cpp/.devcontainer/devenv-setup.sh
+++ vscode/cpp/.devcontainer/devenv-setup.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
-schroot -c bullseye-amd64-sbuild --directory=/ -- bash -c 'echo "deb http://deb.wirenboard.com/wb7/bullseye unstable main" > /etc/apt/sources.list.d/wirenboard-unstable.list'
-schroot -c bullseye-amd64-sbuild --directory=/ -- apt-get update
-schroot -c bullseye-amd64-sbuild --directory=/ -- apt-get -y install gdbserver:armhf gcovr:all j2cli:all
-schroot -c bullseye-amd64-sbuild --directory=/ -- apt-get -y install libwbmqtt1-5-test-utils:armhf libdb5.3++-dev:armhf libsqlite3-dev:armhf
+TARGET="wb8"
 
-dir=$(pwd)
-schroot -c bullseye-amd64-sbuild --directory=/ -- mkdir -p $dir
-echo "$dir  $dir   none    rw,bind         0       0" >> /etc/schroot/sbuild/fstab
+case ${TARGET} in
+    wb8) ARCH=arm64 ;;
+    *)   ARCH=armhf ;;
+esac
+
+DIR=$(pwd)
+CHROOT="schroot -c bullseye-amd64-sbuild --directory=${DIR} --"
+
+echo "${DIR} ${DIR} none rw,bind 0 0" >> /etc/schroot/sbuild/fstab
+
+LIST=$(${CHROOT} dpkg-checkbuilddeps 2>&1 | sed 's/dpkg-checkbuilddeps:\serror:\sUnmet build dependencies: //g' | sed 's/[\(][^)]*[\)] *//g')
+DEPS=()
+
+for ITEM in ${LIST}; do
+    if [[ ${ITEM} != *:all ]]; then
+        ITEM+=":${ARCH}"
+    fi
+    DEPS+=(${ITEM})
+done
+
+${CHROOT} bash -c "echo \"deb http://deb.wirenboard.com/${TARGET}/bullseye unstable main\" > /etc/apt/sources.list.d/wirenboard-unstable.list"
+${CHROOT} apt-get update
+${CHROOT} apt install -y ${DEPS[@]} gdbserver:${ARCH}
 
 apt update
 apt install gdb-multiarch
